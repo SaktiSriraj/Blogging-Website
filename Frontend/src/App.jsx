6115636ff49be9c17ec3c448473b1f209621d843
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-12-02 11:01:41 +0530

    Sign-in and Sign-up routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar.component"
 
 const App = () => {
     return (
-        <Navbar />
+        <Routes>
+            <Route path="/" element={ <Navbar /> }> //Parent: Home + Navbar
+
+                {/*  / + sign-up => /sign-up */}
+                <Route path="sign-in" element={ <h1>Sign in Page</h1> } />
+                <Route path="sign-up" element={ <h1>Sign up Page</h1> } />
+
+            </Route>
+
+        </Routes>
+
     )
 }
 
