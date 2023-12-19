9fc9cf48302ba88a155b25047b59fe7f2c4eec81
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-12-19 12:07:02 +0530

    Form Data Validated

--- Frontend/src/pages/userAuthForm.page.jsx
+++ Frontend/src/pages/userAuthForm.page.jsx
@@ -1,15 +1,64 @@
+import axios from "axios"; //server access
+import { useRef } from "react"; //used to access an HTML element
+import { Toaster, toast } from "react-hot-toast"; //to create alert-ui
+import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
-import { Link } from "react-router-dom";
 
+    
 const UserAuthForm = ({type}) => {
+
+    const authForm = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+        //Form Data
+        let form = new FormData(authForm.current);
+        let formData = {}
+
+        for(let [key, value] of form.entries()) {
+            formData[key] = value;
+        }
+
+        //Destructure formData
+        let { fullname, email, password } = formData
+ 
+        //Form Validations
+        if(fullname){
+            if(fullname.length < 3){
+                return toast.error("Fullname must be ateast 3 letters long")
+            }
+        }
+    
+        if(!email.length){
+            return toast.error("Enter email address");
+        }
+    
+        if(!emailRegex.test(email)){
+            return toast.error("Enter a valid email address");
+        }
+
+        if(!password.length){
+            return toast.error("Enter password");
+        }
+    
+        if(!passwordRegex.test(password)){
+            return toast.error("Password must be at 6 - 20 characters long, with atleast one lowercase character, one uppercase character and a numeric character");
+        }
+
+    }
+
     return (
         <AnimationWrapper keyValue={type}>
 
             <section className="h-cover flex items-center justify-center">
-
-                <form className="w-[80%] max-w-[400px]">
+                <Toaster />
+                <form ref={authForm} className="w-[80%] max-w-[400px]">
 
                     <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                         {type == "sign-in" ? "Welcome Back" : "Join Us Today"}
@@ -41,9 +90,12 @@ const UserAuthForm = ({type}) => {
                         icon="fi-rr-key"
                     />
 
-                    <buton className=" btn-dark mt-14 w-[25%] flex items-center justify-center center " type="submit">
+                    <button
+                        className=" btn-dark mt-14 w-[25%] flex items-center justify-center center "
+                        type="submit" onClick={handleSubmit}>
+
                         {type.replace("-", " ")}
-                    </buton>
+                    </button>
 
                     
                     <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
