25ba8615544ca9379148fe4e74ea6b1b54e02e66
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-12-20 22:49:16 +0530

    Added user navigation panel

--- Frontend/src/components/navbar.component.jsx
+++ Frontend/src/components/navbar.component.jsx
@@ -1,24 +1,38 @@
 import { useContext, useState } from 'react';
 import { Link, Outlet } from "react-router-dom"; // Outlet: Render nested-route elements
-import logo from "../imgs/logo.png";
 import { UserContext } from '../App';
+import logo from "../imgs/logo.png";
 import UserNavigationPanel from './user-navigation.component';
 
 const Navbar = () => {
 
-    const [searchBoxVisibility,setSearchBoxVisibility] = useState (false);
+    const [ searchBoxVisibility,setSearchBoxVisibility ] = useState (false);
+
+    const [ userNavPanel, setUserNavPanel ] = useState (false);
 
     const { userAuth, userAuth: { accessToken, profile_img } } = useContext (UserContext);
+
+    const handleUserNavpanel = () => {
+        setUserNavPanel(currentVal => !currentVal);
+    }
+
+    const handleBlur = () => {
+        setTimeout(() => {
+            setUserNavPanel(false);
+        }, 300);
+    }
 
     return (
         <>
         
             <nav className='navbar'>
 
+                {/* logo */}
                 <Link to="/" className='flex-none w-10'>
                     <img src={logo} className='w-full'/>
                 </Link>
 
+                {/* Search bar for lrge screen */}
                 <div className={'absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ' + (searchBoxVisibility?'show':'hide')}>
                     
                     <input type="text" placeholder='Search' className='w-full md:w-auto bg-grey p-4 pl-6 pr-[-12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 '/>
@@ -29,12 +43,13 @@ const Navbar = () => {
                 
 
                 <div className='flex items-center gap-3 md:gap-6 ml-auto'>
-
+                    
+                    {/* Search bar for small screen */}
                     <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center' onClick={()=>setSearchBoxVisibility(currentVal => !currentVal)}>
                         <i className="fi fi-rr-search text-xl"></i>
                     </button>
 
-
+                    {/* Write button */}
                     <Link to="/editor" className='hidden md:flex gap-2 link rounded-md'>
                         <i className="fi fi-rr-edit"></i>
                         <p>Write</p>
@@ -43,24 +58,34 @@ const Navbar = () => {
                     {
                         accessToken ?
                             <>
-                            
+
+                                {/* notification button */}
                                 <Link to="/dashboard/notification">
                                     <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                                         <i className="fi fi-rr-bell text-2xl block mt-2 text-dark-grey"></i>
                                     </button>
                                 </Link>
 
-                                <div className='relative'>
+                                {/* profile image */}
+                                <div className='relative' onClick={handleUserNavpanel} onBlur={handleBlur}>
                                     <button className='w-12 h-12 mt-1'>
                                         <img src={profile_img} className='w-full h-full object-cover rounded-full' />
                                     </button>
                                     
-                                    <UserNavigationPanel />
+
+                                    {
+                                        userNavPanel ?
+                                            <UserNavigationPanel />
+                                        :
+                                            ""
+                                    }
+                                    
                                 </div>
 
                             
                             </>
                         :
+                            // sign-in and sign-up button
                             <>
                                 <Link className='btn-dark py-2.5' to="/sign-in">
                                     Sign in
--- Frontend/src/components/user-navigation.component.jsx
+++ Frontend/src/components/user-navigation.component.jsx
@@ -19,7 +19,7 @@ const UserNavigationPanel = () => {
             transition={{ duration: 0.2 }}>
             
             <div className="bg-white absolute right-0 border border-grey w-60 duration-200" >
-                <Link to="/editor" className="flex gp-2 link md:hidden pl-8 py-4" >
+                <Link to="/editor" className="flex gap-2 link md:hidden pl-8 py-4" >
                     <i className="fi fi-rr-edit"></i>
                     <p>Write</p>
                 </Link>
